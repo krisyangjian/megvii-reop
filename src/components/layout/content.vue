3311a3b725cd8c2dda0548bf8060847300ad069c
krisyang <krisyangjian@126.com>
2017-11-13 01:51:01 +0800

    sdsd

--- src/components/layout/content.vue
+++ src/components/layout/content.vue
@@ -1,12 +1,76 @@
 <template>
-	<div class="content">content</div>
+	<div class="content">
+		<div>
+			<el-breadcrumb separator-class="el-icon-arrow-right">
+  				<el-breadcrumb-item :to="{ path: '/' }">人像检索</el-breadcrumb-item>
+  				<el-breadcrumb-item>选项1</el-breadcrumb-item>
+			</el-breadcrumb>
+		</div>
+		<div style="float:right;margin-bottom:50px;">
+			<el-button type="primary" icon="el-icon-upload" @click="dialogVisible = true">新建底库</el-button>
+		</div>
+		<div>  
+			<el-table
+    			:data="tableData"
+    			stripe
+    			style="width: 100%">
+    			<el-table-column
+      				prop="date"
+      				label="日期"
+      				width="180">
+    			</el-table-column>
+    			<el-table-column
+      				prop="name"
+      				label="姓名"
+      				width="180">
+    			</el-table-column>
+    			<el-table-column
+      				prop="address"
+      				label="地址">
+    			</el-table-column>
+  			</el-table>
+  		</div>
+
+		<el-dialog
+  			title="新建底库"
+  			:visible.sync="dialogVisible"
+  			width="25%"
+  			:fullscreen="true"
+  			customClass="newLibDialog">
+  			<slot>
+  				<div>
+  					<span>底库名称：</span><el-input placeholder="2-12汉字"></el-input>
+  				</div>
+  			</slot>
+  			<span slot="footer" class="dialog-footer">
+    			<el-button type="primary" @click="dialogVisible = false">完  成</el-button>
+  			</span>
+		</el-dialog>
+  	</div>
 </template>
 <script>
 	export default {
 		name: 'content',
 		data() {
 			return{
-				message: 'yangjian'
+				tableData: [{
+					date: '2016-05-02',
+          			name: '王小虎',
+          			address: '上海市普陀区金沙江路 1518 弄'
+        		}, {
+        			date: '2016-05-04',
+          			name: '王小虎',
+          			address: '上海市普陀区金沙江路 1517 弄'
+        		}, {
+          			date: '2016-05-01',
+          			name: '王小虎',
+          			address: '上海市普陀区金沙江路 1519 弄'
+        		}, {
+          			date: '2016-05-03',
+          			name: '王小虎',
+          			address: '上海市普陀区金沙江路 1516 弄'
+        		}],
+        		dialogVisible: false				
 			}
 		},
 		components: {
@@ -20,6 +84,16 @@
 	.content {
 		width: 100%;
 		height: 100%;
-		border: 1px solid #c7c7ca
+	}
+	.newLibDialog {
+		position: relative;
+	}
+	.newLibDialog .dialog-footer {
+		position: absolute;
+		bottom: 20px;
+		right: 20px;
+	}
+	.newLibDialog .el-input {
+		width: 370px;
 	}
 </style>
--- src/components/layout/side.vue
+++ src/components/layout/side.vue
@@ -1,39 +1,30 @@
 <template>
-	<div class="side">
-		<div style="background-color: blue">menu header</div>
-		<div>
-    		<el-menu :default-openeds="['1']">
+	<div class="sidebar">
+		<div class="collapse-btn" 
+      @click="collapseClick">
+      <i class="el-icon-tickets"></i>
+    </div>
+		<div 
+      :class="{
+        'menu-wrap': true,
+        'close': isClose
+      }">
+    		<el-menu
+          class="menu-megvii"
+          background-color="transparent"
+          text-color="#fff"
+          :collapse="isCollapse"
+          >
       			<el-submenu index="1">
-        			<template slot="title"><i class="el-icon-message"></i>导航一</template>
+        			<template slot="title"><i class="el-icon-caret-bottom"></i><span>人像检索</span></template>
         			<el-menu-item index="1-1">选项1</el-menu-item>
         			<el-menu-item index="1-2">选项2</el-menu-item>
-<!--         			<el-menu-item-group>
-          				<el-menu-item index="1-1">选项1</el-menu-item>
-          				<el-menu-item index="1-2">选项2</el-menu-item>
-        			</el-menu-item-group> -->
-<!--         			<el-menu-item-group title="分组2">
-          				<el-menu-item index="1-3">选项3</el-menu-item>
-        			</el-menu-item-group> -->
-<!--         			<el-submenu index="1-4">
-          				<template slot="title">选项4</template>
-          				<el-menu-item index="1-4-1">选项4-1</el-menu-item>
-        			</el-submenu> -->
       			</el-submenu>
-<!--       			<el-submenu index="2">
-        			<template slot="title"><i class="el-icon-menu"></i>导航二</template>
-        			<el-menu-item-group>
-          				<template slot="title">分组一</template>
-          				<el-menu-item index="2-1">选项1</el-menu-item>
-          				<el-menu-item index="2-2">选项2</el-menu-item>
-        			</el-menu-item-group>
-        			<el-menu-item-group title="分组2">
-          				<el-menu-item index="2-3">选项3</el-menu-item>
-        			</el-menu-item-group>
-        			<el-submenu index="2-4">
-          				<template slot="title">选项4</template>
-          				<el-menu-item index="2-4-1">选项4-1</el-menu-item>
-        			</el-submenu>
-      			</el-submenu> -->
+      			<el-submenu index="2">
+        			<template slot="title"><i class="el-icon-caret-bottom"></i><span>资源管理</span></template>
+          		<el-menu-item index="2-1">选项1</el-menu-item>
+          		<el-menu-item index="2-2">选项2</el-menu-item>oup>
+      			</el-submenu>
     		</el-menu>
 		</div>
 	</div>
@@ -44,23 +35,53 @@
 		name: 'side',
 		data() {
 			return{
-				message: 'yangjian'
+				isClose: false,
+        isCollapse: false
 			}
 		},
 		components: {
 		},
 		methods: {
+      collapseClick() {
+        this.isClose = !this.isClose;
+        // this.isCollapse = !this.isCollapse;
+      }
 		}
 	}	
 </script>
 
 <style>
-	.side .el-menu {
+  .sidebar .menu-wrap{
+    /*-webkit-transition: all .3s;*/
+    overflow: hidden;
+  }
+  .sidebar .menu-wrap.close {
+    /*-webkit-transition: all .3s;*/ 
+    height: 0px;
+  }
+  .sidebar .collapse-btn {
+    height: 42px;
+    line-height: 42px;
+    font-size: 18px;
+    text-align: center;
+    background-color: #424d5a;
+    cursor: pointer;
+    /*display: none;*/
+  }
+  .sidebar .collapse-btn i {
+    color: #fff;
+    margin-left: -9px;
+  }
+	.menu-megvii.el-menu {
 		border-right: none;
-		background-color: transparent;
-		/*border-right: solid 1px #333333;*/
 	}
-	.side .el-submenu .el-menu-item {
+	.menu-megvii .el-submenu .el-menu-item {
 		min-width: 170px;
 	}
+  .menu-megvii .el-submenu__title i {
+    color: #fff;
+  }
+  .menu-megvii .el-submenu__title .el-submenu__icon-arrow {
+    display: none;
+  }
 </style>
